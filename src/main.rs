db7ce30742792d5aff7eb430dc563ba63897cbc2
Jacob Stovall <jacobemmersonstovall@gmail.com>
2021-03-20 12:51:43 -0700

    Reading filename from the command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
+fn get_filename() -> String {
+    let args : Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("The wrong number of arguments were given");
+    }
+    String::from(&args[1])
+}
+
 fn main() {
-    println!("Hello, world!");
+    let filename = get_filename();
+    println!("Filename: {}", filename);
 }
