98301084e60e271e8e968b321d2dfca479294742
Jacob Stovall <jacobemmersonstovall@gmail.com>
2021-07-08 12:22:57 -0700

    Cleaned up code a bit in assembler file

--- src/assembler.rs
+++ src/assembler.rs
@@ -6,21 +6,23 @@ use crate::symbol_table::SymbolTable;
 fn clean_up_line(line: &String) -> String {
     let without_whitespace = line.split_whitespace().collect::<Vec<&str>>();
     let without_whitsepace_string = without_whitespace.join("").to_string();
-    let index_of_comment = without_whitsepace_string.find("//").unwrap_or(usize::MAX);
-    let mut return_string = without_whitsepace_string;
-    if index_of_comment != usize::MAX {
-        let return_str = &return_string[..index_of_comment];
-        return_string = return_str.to_string();
+
+    let index_of_comment = without_whitsepace_string.find("//");
+    match index_of_comment {
+        Some(value) => String::from(&without_whitsepace_string[..value]),
+        None => without_whitsepace_string,
     }
-    return_string
 }
 
 pub fn assemble_from_string(code_to_assemble: &String) {
     let lines = code_to_assemble.split("\n").collect::<Vec<&str>>();
+    let mut clean_code_lines = Vec::new();
     for line in lines {
         let cleaned_up_line = &clean_up_line(&line.to_string());
         if cleaned_up_line != "" {
-            println!("{}", cleaned_up_line);
+            clean_code_lines.push(String::from(cleaned_up_line));
         }
     }
+
+    println!("{:?}", clean_code_lines);
 }
